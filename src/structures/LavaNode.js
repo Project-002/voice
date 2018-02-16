775190208f666e136313dae3fd09ebd991160170
Frangu Vlad <contact@kingdgrizzle.ml>
2018-02-16 22:27:29 +0200

    docs: Document everything (#1)

--- src/structures/LavaNode.js
+++ src/structures/LavaNode.js
@@ -1,15 +1,77 @@
 const { EventEmitter } = require('events');
 const WebSocket = require('ws');
 
+/**
+ * A LavaNode instance
+ * @extends {EventEmitter}
+ */
 class LavaNode extends EventEmitter {
+	/**
+	 * Options passed to LavaNode when creating a new instance
+	 * @typedef {object} LavaNodeOptions
+	 * @prop {string} [gateway] The gateway URL
+	 * @prop {string} [user] The user
+	 * @prop {number} [shards=1] The shard count
+	 * @prop {string} [password] The password
+	 * @prop {number} [reconnectInterval=10000] The reconnect interval, in milliseconds
+	 */
+
+	/**
+	 * Creates an instance of LavaNode.
+	 * @param {LavaNodeOptions} [options={}] The node options
+	 * @memberof LavaNode
+	 */
 	constructor(options = {}) {
 		super();
+		/**
+		 * The WebSocket URL
+		 * @type {string}
+		 */
 		this.gateway = `ws://${options.gateway}`;
+
+		/**
+		 * The user ID of this node
+		 * @type {string}
+		 */
 		this.user = options.user;
+
+		/**
+		 * The shard amount
+		 * @type {number}
+		 * @default 1
+		 */
 		this.shards = options.shards || 1;
+
+		/**
+		 * The password used when connecting to the WebSocket
+		 * @type {string}
+		 */
 		this.password = options.password;
+
+		/**
+		 * The reconnect interval, in milliseconds
+		 * @type {number}
+		 * @default 10000
+		 */
 		this.reconnectInterval = options.reconnectInterval || 10000;
+
+		/**
+		 * The WebSocket connection
+		 * @type {?WebSocket}
+		 */
 		this.ws = null;
+
+		/**
+		 * The statistics of this node
+		 * @typedef {object} LavaNodeStatistics
+		 * @prop {number} [players=0] The amount of connected players
+		 * @prop {number} [playingPlayers=0] The amount of currently playing players
+		 */
+
+		/**
+		 * The current stats of this LavaNode
+		 * @type {LavaNodeStatistics}
+		 */
 		this.stats = {
 			players: 0,
 			playingPlayers: 0
@@ -18,6 +80,11 @@ class LavaNode extends EventEmitter {
 		this._connect();
 	}
 
+	/**
+	 * Connects to the WebSocket.
+	 * @private
+	 * @memberof LavaNode
+	 */
 	_connect() {
 		this.ws = new WebSocket(this.gateway, {
 			headers: {
@@ -33,43 +100,111 @@ class LavaNode extends EventEmitter {
 		this.ws.on('error', this._error.bind(this));
 	}
 
+	/**
+	 * Function ran when the WebSocket connection is ready.
+	 * @private
+	 * @memberof LavaNode
+	 */
 	_ready() {
+		/**
+		 * A value representing if the node is currently ready
+		 * @type {boolean}
+		 */
 		this.ready = true;
+
+		/**
+		 * Emmited when the node gets ready
+		 * @event LavaNode#ready
+		 */
 		this.emit('ready');
 	}
 
+	/**
+	 * Parses a received message.
+	 * @private
+	 * @param {object} message The WebSocket message
+	 * @returns {void}
+	 * @memberof LavaNode
+	 */
 	_message(message) {
 		try {
 			var data = JSON.parse(message);
 		} catch (error) {
+			/**
+			 * Emmited when an error occured
+			 * @event LavaNode#error
+			 * @prop {Error|TypeError|RangeError} error The error
+			 */
 			return this.emit('error', error);
 		}
 		if (!data) return undefined;
 		if (data.op === 'stats') this.stats = data;
 
+		/**
+		 * Emmited when a message is received and parsed
+		 * @event LavaNode#message
+		 * @prop {object} data The raw message data
+		 */
 		return this.emit('message', data);
 	}
 
+	/**
+	 * Handles any errors that occur.
+	 * @private
+	 * @param {Error} error The received error
+	 * @returns {void}
+	 * @memberof LavaNode
+	 */
 	_error(error) {
 		if (error.message.includes('socket hang up')) return undefined;
 		if (error.message.includes('ECONNREFUSED')) return this._reconnect();
 		return this.emit('error', error);
 	}
 
+	/**
+	 * Function ran when the WebSocket connection closes.
+	 * @private
+	 * @param {number} code The code used for closing the connection
+	 * @param {any} reason The reason for closing the connection
+	 * @returns {void}
+	 * @memberof LavaNode
+	 */
 	_close(code, reason) {
 		this.ready = false;
 		if (code !== 1000) return this._reconnect();
 		this.ws = null;
+
+		/**
+		 * Emmited when the node disconnects from the WebSocket,
+		 * and won't attempt to reconnect
+		 * @event LavaNode#disconnect
+		 * @prop {string} reason The reason for the disconnect
+		 */
 		return this.emit('disconnect', reason);
 	}
 
+	/**
+	 * Handles the reconnecting to the WebSocket.
+	 * @private
+	 * @memberof LavaNode
+	 */
 	_reconnect() {
 		setTimeout(() => {
+			/**
+			 * Emmited when the node is attempting a reconnect
+			 * @event LavaNode#reconnecting
+			 */
 			this.emit('reconnecting');
 			this._connect();
 		}, this.reconnectInterval);
 	}
 
+	/**
+	 * Sends a packet to the WebSocket connection.
+	 * @param {object} data The data to send
+	 * @returns {void}
+	 * @memberof LavaNode
+	 */
 	send(data) {
 		try {
 			var payload = JSON.stringify(data);
--- src/structures/LavaPlayer.js
+++ src/structures/LavaPlayer.js
@@ -1,19 +1,99 @@
 const { EventEmitter } = require('events');
 
+/**
+ * A LavaPlayer instance
+ * @extends {EventEmitter}
+ */
 class LavaPlayer extends EventEmitter {
+	/**
+	 * Options passed to LavaPlayer when creating a new instance
+	 * @typedef {object} LavaPlayerOptions
+	 * @prop {LavaNode} [node] The node used for this player
+	 * @prop {string} [guild] The guild ID of this player
+	 * @prop {string} [channel] The channel ID of this player
+	 */
+
+	/**
+	 * Creates an instance of LavaPlayer.
+	 * @param {LavaPlayerOptions} [options={}] The player options
+	 * @memberof LavaPlayer
+	 */
 	constructor(options = {}) {
-		super(options);
+		super();
+		/**
+		 * The node used for this player
+		 * @type {LavaNode}
+		 */
 		this.node = options.node;
+
+		/**
+		 * The guild ID of this player
+		 * @type {string}
+		 */
 		this.guild = options.guild;
+
+		/**
+		 * The channel ID of this player
+		 * @type {string}
+		 */
 		this.channel = options.channel;
+
+		/**
+		 * If this player is ready
+		 * @type {boolean}
+		 * @default false
+		 */
 		this.ready = false;
+
+		/**
+		 * If this player is currently playing
+		 * @type {boolean}
+		 * @default false
+		 */
 		this.playing = false;
+
+		/**
+		 * If this player is currently paused
+		 * @type {boolean}
+		 * @default false
+		 */
 		this.paused = false;
+
+		/**
+		 * The current state of this player
+		 * TODO: Needs more docs
+		 * @type {object}
+		 * @default {}
+		 */
 		this.state = {};
+
+		/**
+		 * The current track
+		 * @type {?string}
+		 * @default null
+		 */
 		this.track = null;
+
+		/**
+		 * The start time of this player
+		 * @type {Date}
+		 * @default Date.now()
+		 */
 		this.timestamp = Date.now();
 	}
 
+	/**
+	 * Options passed when connecting to a voice channel using a {@link LavaPlayer} instance
+	 * @typedef {object} LavaPlayerConnectData
+	 * @prop {string} [session] The session ID
+	 * @prop {string} [event] The event
+	 */
+
+	/**
+	 * Connects to a voice channel.
+	 * @param {LavaPlayerConnectData} data The connection data
+	 * @memberof LavaPlayer
+	 */
 	connect(data) {
 		this.node.send({
 			op: 'voiceUpdate',
@@ -23,6 +103,12 @@ class LavaPlayer extends EventEmitter {
 		});
 	}
 
+	/**
+	 * Plays a track in the channel.
+	 * @param {string} track The track that should be played
+	 * @param {object} options The options for playing the track
+	 * @memberof LavaPlayer
+	 */
 	play(track, options) {
 		this.track = track;
 		const payload = Object.assign({
@@ -35,6 +121,10 @@ class LavaPlayer extends EventEmitter {
 		this.timestamp = Date.now();
 	}
 
+	/**
+	 * Stops playing the current track.
+	 * @memberof LavaPlayer
+	 */
 	stop() {
 		this.node.send({
 			op: 'stop',
@@ -44,6 +134,12 @@ class LavaPlayer extends EventEmitter {
 		this.track = null;
 	}
 
+	/**
+	 * Pauses the playing of the current track.
+	 * @param {?boolean} [pause] If the playback should be paused or resumed
+	 * @returns {void}
+	 * @memberof LavaPlayer
+	 */
 	pause(pause) {
 		if ((pause && this.paused) || (!pause && !this.paused)) return;
 		this.node.send({
@@ -54,6 +150,11 @@ class LavaPlayer extends EventEmitter {
 		this.paused = Boolean(pause);
 	}
 
+	/**
+	 * Changes the volume of the current player.
+	 * @param {string} volume The new volume
+	 * @memberof LavaPlayer
+	 */
 	volume(volume) {
 		this.node.send({
 			op: 'volume',
@@ -62,6 +163,11 @@ class LavaPlayer extends EventEmitter {
 		});
 	}
 
+	/**
+	 * Seeks the playback to the specified time.
+	 * @param {string} position The new position
+	 * @memberof LavaPlayer
+	 */
 	seek(position) {
 		this.node.send({
 			op: 'seek',
@@ -70,19 +176,44 @@ class LavaPlayer extends EventEmitter {
 		});
 	}
 
+	/**
+	 * Function ran when playback ends.
+	 * @param {object} message The raw message object
+	 * @memberof LavaPlayer
+	 */
 	end(message) {
 		if (message.reason !== 'REPLACED') {
 			this.playing = false;
 			this.track = null;
 		}
 
+		/**
+		 * Emmited when the player ends playing
+		 * @event LavaPlayer#end
+		 * @prop {object} message The raw message
+		 */
 		this.emit('end', message);
 	}
 
+	/**
+	 * Function that is ran when an error is occured.
+	 * @param {object} message The raw message
+	 * @memberof LavaPlayer
+	 */
 	exception(message) {
+		/**
+		 * Emmited when the player encounters an error
+		 * @event LavaPlayer#error
+		 * @prop {object} message The raw message
+		 */
 		this.emit('error', message);
 	}
 
+	/**
+	 * Function that stops the player when it gets stuck.
+	 * @param {object} message The raw message
+	 * @memberof LavaPlayer
+	 */
 	stuck(message) {
 		this.stop();
 		this.emit('end', message);
